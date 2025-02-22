f449b30a4103e99e1d596857e2bb7e5007e09775
mickias-zerfu <mickiaszerfu@gmail.com>
2025-02-22 08:30:20 +0300

    build commit final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,34 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import * as AOS from 'aos';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from "./home/home.component";
-import { OurStoryComponent } from "./our-story/our-story.component";
-import { WeddingDetailsComponent } from "./wedding-details/wedding-details.component";
-import { RegistryComponent } from "./registry/registry.component";
-import { SubscribeComponent } from "./subscribe/subscribe.component";
-import { AboutUsComponent } from "./about-us/about-us.component";
-import { GalleryComponent } from "./gallery/gallery.component"; // ✅ Import HeaderComponent
+import { HomeComponent } from './home/home.component';
+import { OurStoryComponent } from './our-story/our-story.component';
+import { WeddingDetailsComponent } from './wedding-details/wedding-details.component';
+import { RegistryComponent } from './registry/registry.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { FooterComponent } from './footer/footer.component'; // ✅ Import HeaderComponent
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, OurStoryComponent, WeddingDetailsComponent, RegistryComponent, SubscribeComponent, AboutUsComponent, GalleryComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    OurStoryComponent,
+    WeddingDetailsComponent,
+    RegistryComponent,
+    SubscribeComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit, AfterViewInit {
-
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
@@ -26,11 +37,12 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.route.fragment.subscribe(fragment => {
+    this.route.fragment.subscribe((fragment) => {
       if (fragment) {
-        document.getElementById(fragment)?.scrollIntoView({ behavior: 'smooth' });
+        document
+          .getElementById(fragment)
+          ?.scrollIntoView({ behavior: 'smooth' });
       }
     });
   }
 }
-
--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -6,79 +6,89 @@ import { BrowserModule } from '@angular/platform-browser';
 @Component({
   selector: 'app-registry',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './registry.component.html',
-  styleUrl: './registry.component.css'
+  styleUrl: './registry.component.css',
 })
 export class RegistryComponent {
   guestName: string = '';
   guestMessage: string = '';
-  messages: { name: string; text: string; color: string }[] = [
+  messages: { name: string; src: string; text: string; color: string }[] = [
     {
-      "name": "Mikiyas Z.",
-      "text": "To the honored members of Yegeta Lijoch : The developer of this site is seeking a blessed marriage. Please reach out if interested.!",
-      "color": "#FFD3B5"
+      name: 'ጌታባለው.',
+      src: 'assets/images/thanks/a.jpg',
+      text: 'ጌት ስለአንተ ስናስብ ስለማረፊያችን ነው የምናስበው። እግዚአብሔር በአንተ በብዙ መንገድ አሳርፎናል። ገና ከጅምሩ አንዳንድ ነገር እንዴት ሊሆን ነው ብለን መጨነቅ ሳንጀምር አንተ ጋር መፍትሄው አለ።በጥቂት ቃላት ግለጹት ብንባል " ሰው እንዴት ጓደኛም ቤተሰብም አለቃም መሆን ይችላል?" ነው ማለት የምንችለው። ስለአንተ ቸሩ ይመስገን',
+      color: '#FFD3B5',
     },
     {
-      "name": "Hana T.",
-      "text": "As you begin this beautiful journey together, may the Lord be the foundation of your home. Wishing you a lifetime of love and blessings!",
-      "color": "#FFAAA6"
+      name: 'በርይሁን እና ማስተዋል.',
+      src: 'assets/images/thanks/b.jpg',
+      text: 'እንደምታውቁት ነው አንድ ዘመድ ጥሩ ብንባል እናንተ ናችሁ ያላችሁን። በስጋ በምንም ሳንዛመድ እንዴት ነው እሱ እንዲህ የተዋሃድነው? በርዬ ደርባባ ወንድም ጋሼ ነህ ማስቲዬ በዚህም ዘመን ንጹህ እና ቅን ሰው አለ ታስብዪናለሽ። ከቤታችሁ ጓዳ ከልባችሁ ፍቅር ምን ጎደለብን እግዚአብሔር መልካሙን ሁሉ ይሙላባችሁ',
+      color: '#FFAAA6',
     },
     {
-      "name": "Tesfaye Alemu",
-      "text": "May your love grow stronger each day, and may God be your guide in every step. Wishing you a blessed and joyful marriage!",
-      "color": "#D5AAFF"
+      name: 'ታሜ እና መሲ',
+      src: 'assets/images/thanks/c.jpg',
+      text: 'እኛን ወዳጅ ስለማድረግ ወረድ አላችሁልን ለእኛ ጊዜ ኖራችሁ ልብ ኖራችሁ በሃሳባቹ ስፍራ ሰጣችሁን ስለዚህ በምድር ሊገለጽ ከሚችለው ምን ነሳችሁን? ምንም! መሲዬ ሁሌ እንደምንልሽ ከጅማሬው እስከ ፍጻሜ ወገባቹን ታጥቃችሁ ዳራችሁን። በእውነት ስለፍቅራችሁ እና ቅንነታችሁ ቃል የለንም። በቃ እንወዳቿለን።',
+      color: '#D5AAFF',
     },
     {
-      "name": "Mekdes & Dawit",
-      "text": "May your marriage reflect Christ’s love for the Church. Wishing you a lifetime of peace, faith, and happiness!",
-      "color": "#A2D5F2"
+      name: 'ኤርሚ እና ሕሉ',
+      src: 'assets/images/thanks/d.jpg',
+      text: 'በእውነት በትክክል ልከኛ የሆነውን ነገር አስተማራችሁን። ታዲያ ከቃሉ ብቻ አይደለም ከሕይወትም። ያለስስት በልግስና ካላችሁ ሁሉ ምክር አንድም አልቀረ መግባችሁናል። እንደ ህጻን ልጅ እጃችንን ይዛችሁ የመምራት ያህል መራችሁን። መንገዳችንን አቀለላችሁልን። በእውነት እንወዳቿለን።',
+      color: '#A2D5F2',
     },
     {
-      "name": "Yohannes Gebre",
-      "text": "Congratulations! May God be the center of your marriage and fill your home with His divine presence and unshakable joy.",
-      "color": "#F5D76E"
+      name: 'ሚዙ እና ማኔ',
+      src: 'assets/images/thanks/e.jpg',
+      text: 'እናንተን ያስተዋወቀችን ቤተክርስቲያን  ደግሞም የሰጠንን እግዚአብሔርን እናመሰግናለን።ከረጅሙ የሕይወት ጉዞ የትዳር መንገዳችሁ ልምዳችሁን ሁሉ ሰጣችሁን መከራችሁን በቤታችሁ አስተናገዳችሁን ደግሞም በየጊዜው ተከታተላችሁን። እናመሰግናቿለን።',
+      color: '#F5D76E',
     },
     {
-      "name": "Selamawit A.",
-      "text": "May your love be as deep as the ocean and your faith as strong as the mountains. Wishing you a blessed and wonderful life together!",
-      "color": "#FF9AA2"
+      name: 'አማኑ እና ሰርክ',
+      src: 'assets/images/thanks/f.jpg',
+      text: 'ሰው በጌታ ኢየሱስ ስላመነ ብቻ ቤተሰብ የምታደርጉ ጓዳችሁን ከፍታቹ ስታስገቡን የገባን ነገር ቀድሞ ልባችሁ እንደተከፈተልን ነው። እግዚአብሔር የምትሹትን ሁሉ የምታገኙበትን ደጁን ወለል አድርጎ ይክፈትላችሁ እንጂ ምን ይባላል',
+      color: '#FF9AA2',
     },
     {
-      "name": "Kaleab & Betel",
-      "text": "May the Lord bless and protect your union, and may your love be a testimony of His grace. Wishing you happiness and peace!",
-      "color": "#FFB7B2"
+      name: 'ዳጊ እና ናቲ',
+      src: 'assets/images/thanks/image.jpg',
+      text: 'ጓደኝነታችን ቤተሰባዊነታችን በምን ይለካ ይሆን? አምላካዊ በሆነ ፍቅር በመካከላችን በሚሰራ ሕይወቱ ተሳስረን እንጂ ይህን ያህል ሊኮንልን የሚገባን አድርገን አንወስድም ራሳችንን እንደሚገባን አንቆጥርም።ከግንኙነታችን ሁሉ የምንወደው ነገር ምንም ነገር እንደውለታ የማንቆጣጠረው ነገር ነው።ባስፈለገን ነገር ሁሉ ባስፈለገን ጊዜ ሁሉ ራሳችንን ያለ ልክ እየሰጠን በመተሳሰብ የኖርንባቸውን አመታት አምላክ ይባርክልን።',
+      color: '#FFB7B2',
     },
     {
-      "name": "Elias & Mihret",
-      "text": "May your marriage be a shining light for others, full of love, patience, and faith in Christ. Congratulations and God bless!",
-      "color": "#FFDAC1"
-    },
-    {
-      "name": "Binyam Samuel",
-      "text": "May the Lord be your foundation, your guide, and your strength. Wishing you a joyful marriage filled with divine blessings!",
-      "color": "#B5EAD7"
-    },
-    {
-      "name": "Ruth & Henok",
-      "text": "A threefold cord is not easily broken (Ecclesiastes 4:12). May your marriage be strengthened by the presence of Christ always!",
-      "color": "#C7CEEA"
+      name: 'ቲጂዬ',
+      src: 'assets/images/thanks/tg.jpg',
+      text: 'ቅድም አግኝተንሽ ግን ለአመታት የዘለቀ ወዳጅነት እንዳለን እንዲሰማን ያደረግሽበት ጥበብ ደግሞም ለሕይወት ዘመን የሚሆነን ስንቅ ለማሰነቅ ስትደክሚ ለሚያይሽ ሰው ከዛ ሁሉ ሃላፊነት አጣበሽ የመጣሽ ከዛ ሁሉ የሕይወት ልምድ ለኛ ለትንንሾቹ ለማካፈል የወረድሽ አትመስዪም። ደግሞ በእኛ ፊት ስለ ሁሉ ግልጽ ሆነሽልን አስተዋይ አደረግሽን። ከመጣነው መንገድ ይልቅ የምንሄደውን ስናየው እንዴት እንደሚያጓጓን በቃ እንወድሻለን',
+      color: '#FFDAC1',
     }
-  ]
-  ;
+    
+  ];
 
   pastelColors = [
-    '#FFD3B5', '#FFAAA6', '#D5AAFF', '#A2D5F2', '#F5D76E', 
-    '#FF9AA2', '#FFB7B2', '#FFDAC1', '#B5EAD7', '#C7CEEA'
+    '#FFD3B5',
+    '#FFAAA6',
+    '#D5AAFF',
+    '#A2D5F2',
+    '#F5D76E',
+    '#FF9AA2',
+    '#FFB7B2',
+    '#FFDAC1',
+    '#B5EAD7',
+    '#C7CEEA',
   ];
 
   addMessage(event: Event): void {
     event.preventDefault();
     if (this.guestName.trim() && this.guestMessage.trim()) {
-      const randomColor = this.pastelColors[Math.floor(Math.random() * this.pastelColors.length)];
-      this.messages.unshift({ name: this.guestName, text: this.guestMessage, color: randomColor });
+      const randomColor =
+        this.pastelColors[Math.floor(Math.random() * this.pastelColors.length)];
+      this.messages.unshift({
+        name: this.guestName,
+        src: 'assets/images/thanks/h.jpg',
+        text: this.guestMessage,
+        color: randomColor,
+      });
       this.guestName = '';
       this.guestMessage = '';
     }
